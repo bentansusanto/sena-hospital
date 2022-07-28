beebc55add2bd6dd5975f50e6dcc36c5b8acc59d
bentansusanto <tansusanto194@gmail.com>
2022-07-28 20:32:55 +0700

    first

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import homeView from '../components/homeView'
+
+
+const routes = [
+        {
+            path :'/',
+            name : 'Homepage',
+            components : homeView
+        }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+  })
+
+router.beforeEach((to, from, next) => {
+    console.log(to);
+    document.title = `${to.name} - ${process.env.VUE_APP_TITLE} `
+    next()
+ })
+
+export default router
